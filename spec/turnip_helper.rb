1523a5440e7d4fe9bb4324e4b715148960e5e82b
Joe Fiorini <joe@joefiorini.com>
2013-01-07 08:27:36 -0500

    Remove tmpdir before running tests
    Need to make sure tmpdir isn't corrupted by previous tests with same
    site name

--- spec/turnip_helper.rb
+++ spec/turnip_helper.rb
@@ -11,4 +11,9 @@ end
 RSpec.configure do |config|
   config.include RunSteps
   config.include StaticlySteps
+
+  config.before :each do
+    @aruba_timeout_seconds = 10
+    FileUtils.rm_rf File.expand_path("~/.staticly/tmpcache")
+  end
 end
