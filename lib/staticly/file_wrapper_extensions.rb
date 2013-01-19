331a3d86c8a57b30073a8e3480198d8a95ecc383
Joe Fiorini <joe@joefiorini.com>
2013-01-19 14:04:04 -0500

    Add missing file

---
+++ lib/staticly/file_wrapper_extensions.rb
@@ -0,0 +1,13 @@
+module Staticly
+  module FileWrapperExtensions
+    
+    def final_output=(output)
+      @final_output = output
+    end
+
+    def final_output
+      @final_output
+    end
+
+  end
+end
